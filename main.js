01a50ee40b97f440d3fbc387ea7e638fa3437caa
Marcelo Peruzzo <marceloperuzzo@ampernet.com.br>
2022-06-06 15:47:08 -0300

    FinalizaçãoJS

--- main.js
+++ main.js
@@ -4,7 +4,76 @@ onScroll()
 function onScroll(){
     shownavOnScroll()
     showBackToButton()
+
+    activateMenuAtCurrentSection(home)
+    activateMenuAtCurrentSection(services)
+    activateMenuAtCurrentSection(about)
+    activateMenuAtCurrentSection(contact)
+
 }
+
+function activateMenuAtCurrentSection(section){
+    //linha alvo
+    const targetLine = scrollY + innerHeight / 2
+
+
+//verificar se a seção passou da linha
+//quais dados vou precisar?
+
+//o topo da seção
+    const sectionTop = section.offsetTop
+
+ // a altura da seção   
+    const sectionHeight = section.offsetHeight
+
+ //o topo da seção chegou ou ultrapassou a linha alvo   
+    const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+ //informativo/testes   
+    //console.log('O topo da seção chegou ou passou da linha?', sectionTopReachOrPassedTargetLine)
+
+ //verificar se a base está abaixo da linha alvo
+    const sectionEndsAt = sectionTop + sectionHeight    
+
+    const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+    //console.log('o fundo da seção passou da linha?', sectionEndPassedTargetLine)
+
+ //limites da seção
+    const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine  
+ 
+ //carregar o seletor ID "TODOS QUE ESTEJA NO HREF"do html para o JS
+    const sectionId = section.getAttribute('id')
+    const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+ 
+ //sempre remove a classe active, caso entrar no IF adicione a classe.    
+    menuElement.classList.remove('active')
+    if(sectionBoundaries){
+        menuElement.classList.add('active')
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function shownavOnScroll(){
     if(scrollY > 0){
